8f30c863188477b84b8162e21df786b234684a4a
ERNEST CHRIS <ernestchris6@gmail.com>
2023-10-03 23:22:35 +0100

    manage section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Features from './components/Features';
 
 
     const App = () => {
@@ -8,6 +9,7 @@ import Hero from './components/Hero';
     <div>
           <Navbar />
           <Hero />
+          <Features />
           
     </div>
   )
