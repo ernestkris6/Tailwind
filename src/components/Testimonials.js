596bce3d4e900adfb00e052dadeff831a0821165
ERNEST CHRIS <ernestchris6@gmail.com>
2023-10-04 12:03:17 +0100

    CTA Button

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -10,6 +10,11 @@ import img5 from '../Assets/avatar-richard.png'
  const Testimonials = ()=> {
   return (
     <>
+    <div className='max-w-6xl px-5 mx-auto mt-32 text-center'>
+            <h2 className='text-4xl font-bold text-center'>
+                What's Different About Manage?
+            </h2>
+    </div>
     <div className='flex flex-col mt-24 md:flex-row md:space-x-6'>
         <div className='flex flex-col items-center p-6 space-y-6
         rounded-lg bg-veryLightGray md:w-1/3'>
@@ -30,6 +35,11 @@ import img5 from '../Assets/avatar-richard.png'
                 "We have been able to cancel so many other transactions since using Manage.
                 There is no more cross-channel confusion and everyone is much more focused."
             </p>
+
+            <a
+                href='#'
+                className='hidden px-6 py-3 pt-2 text-white 
+                bg-brightRed rounded-full baseline hover:bg-darkGrayishBlue md:block'>Get Started</a>
             
 
         </div>
